21811c3700367d1ddcf82034bebcf5371116cd22
roozbehz2 <roozbehz73@gmail.com>
2024-10-20 09:15:18 +0330

    make font and direction

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:circle_stepper/FilDirection.dart';
 import 'package:circle_stepper/circle_stepper.dart';
 import 'package:flutter/material.dart';
 
@@ -19,29 +20,61 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         body: SafeArea(
-          child: Container(
-            height: 300,
-            margin: EdgeInsets.symmetric(horizontal: 16),
-            child: CircleStepper(
-              fillDirection: FillDirection.clockwise,
-              step: 1,
-              totalSteps: 4,
-              betweenStepText: 'of',
-              stepTitles: const {
-                1: 'Step 1',
-                2: 'Step 2',
-                3: 'Step 3',
-                4: 'Step 4',
-              },
-              stepDescriptions: const {
-                1: 'Description 1',
-                2: 'Description 2',
-                3: 'Description 3',
-                4: 'Description 4',
-              },
-              progressColor: Colors.amber,
-              backgroundColor: Colors.grey.withAlpha(100),
-            ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: 150,
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                child: CircleStepper(
+                  fillDirection: FillDirection.clockwise,
+                  step: 1,
+                  totalSteps: 4,
+                  betweenStepText: 'of',
+                  stepTitles: const {
+                    1: 'Step 1',
+                    2: 'Step 2',
+                    3: 'Step 3',
+                    4: 'Step 4',
+                  },
+                  stepDescriptions: const {
+                    1: 'Description 1',
+                    2: 'Description 2',
+                    3: 'Description 3',
+                    4: 'Description 4',
+                  },
+                  progressColor: Colors.amber,
+                  backgroundColor: Colors.grey.withAlpha(50),
+                ),
+              ),
+              Container(
+                height: 100,
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                child: Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: CircleStepper(
+                    fillDirection: FillDirection.counterClockwise,
+                    step: 1,
+                    totalSteps: 4,
+                    betweenStepText: 'از',
+                    stepTitles: const {
+                      1: 'مرحله 1',
+                      2: 'مرحله 2',
+                      3: 'مرحله 3',
+                      4: 'مرحله 4',
+                    },
+                    stepDescriptions: const {
+                      1: 'توضیحات 1',
+                      2: 'توضیحات 2',
+                      3: 'توضیحات 3',
+                      4: 'توضیحات 4',
+                    },
+                    progressColor: Colors.amber,
+                    backgroundColor: Colors.grey.withAlpha(50),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/circle_stepper.dart
+++ lib/circle_stepper.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 
 import 'FilDirection.dart';
 
-
+import 'package:flutter/material.dart';
+import 'FilDirection.dart';
 
 class CircleStepper extends StatefulWidget {
   final double? width;
@@ -22,7 +23,7 @@ class CircleStepper extends StatefulWidget {
   final Color colorTextStepper;
   final Color colorTitle;
   final Color colorDescriptions;
-  final FillDirection fillDirection; // New property
+  final FillDirection fillDirection;
   final double? strokeWidth;
 
   const CircleStepper({
@@ -42,7 +43,7 @@ class CircleStepper extends StatefulWidget {
     this.colorTextStepper = Colors.black,
     this.colorTitle = Colors.black,
     this.colorDescriptions = Colors.grey,
-    this.fillDirection = FillDirection.clockwise, // Default is clockwise
+    this.fillDirection = FillDirection.clockwise,
     this.strokeWidth,
   }) : super(key: key);
 
@@ -59,37 +60,40 @@ class _CircleStepperState extends State<CircleStepper> with SingleTickerProvider
   void initState() {
     super.initState();
 
+    _initializeAnimation();
+  }
+
+  void _initializeAnimation() {
     _animationController = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 800),
     );
 
-    _progressAnimation = Tween<double>(
-      begin: _previousProgress,
-      end: widget.step / widget.totalSteps,
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.easeInOut,
-    ));
-
+    _setAnimationProgress(widget.step / widget.totalSteps);
     _animationController.forward();
   }
 
-  @override
-  void didUpdateWidget(covariant CircleStepper oldWidget) {
-    super.didUpdateWidget(oldWidget);
-
-    double newProgress = widget.step / widget.totalSteps;
-
+  void _setAnimationProgress(double newProgress) {
     _progressAnimation = Tween<double>(
-      begin: _progressAnimation.value,
+      begin: _previousProgress,
       end: newProgress,
     ).animate(CurvedAnimation(
       parent: _animationController,
       curve: Curves.easeInOut,
     ));
 
-    _animationController.forward(from: 0);
+    _previousProgress = newProgress;
+  }
+
+  @override
+  void didUpdateWidget(covariant CircleStepper oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.step != oldWidget.step || widget.totalSteps != oldWidget.totalSteps) {
+      double newProgress = widget.step / widget.totalSteps;
+      _setAnimationProgress(newProgress);
+      _animationController.forward(from: 0);
+    }
   }
 
   @override
@@ -100,81 +104,92 @@ class _CircleStepperState extends State<CircleStepper> with SingleTickerProvider
 
   @override
   Widget build(BuildContext context) {
+    final double width = widget.width ?? MediaQuery.sizeOf(context).height * .061;
+    final double height = widget.height ?? MediaQuery.sizeOf(context).height * .061;
+
     return SizedBox(
-      height: widget.height ?? MediaQuery.sizeOf(context).height * .061,
+      height: height,
       child: Row(
         children: [
           Stack(
             alignment: Alignment.center,
             children: [
-              SizedBox(
-                width: widget.width ?? MediaQuery.sizeOf(context).height * .061,
-                height: widget.height ?? MediaQuery.sizeOf(context).height * .061,
-                child: RotatedBox(
-                  quarterTurns: 90,
-                  child: AnimatedBuilder(
-                    animation: _progressAnimation,
-                    builder: (context, child) {
-                      // Apply a transform based on the selected direction
-                      return Transform(
-                        transform: widget.fillDirection == FillDirection.clockwise
-                            ? Matrix4.identity() // No transformation for clockwise
-                            : Matrix4.rotationY(3.14159), // Rotate 180 degrees for counter-clockwise
-                        alignment: Alignment.center,
-                        child: CircularProgressIndicator(
-                          color: widget.progressColor,
-                          backgroundColor: widget.backgroundColor,
-                          value: _progressAnimation.value,
-                          strokeWidth: widget.strokeWidth ?? 6,
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-              Positioned(
-                child: Text(
-                  '${widget.step} ${widget.betweenStepText} ${widget.totalSteps}',
-                  style: widget.textStyleStepper ??
-                      TextStyle(
-                        fontSize: MediaQuery.of(context).size.width * .03,
-                        fontWeight: FontWeight.w500,
-                        color: widget.colorTextStepper,
-                        fontFamily: 'Poppins',
-                      ),
-                ),
-              ),
+              _buildCircularProgressIndicator(width, height),
+              _buildProgressText(context),
             ],
           ),
           const SizedBox(width: 18),
-          SizedBox(
-            height: widget.height ?? MediaQuery.of(context).size.height * .061,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  widget.stepTitles[widget.step] ?? '',
-                  style: widget.textStyleTitle ??
-                      TextStyle(
-                        fontSize: MediaQuery.of(context).size.width * .034,
-                        fontWeight: FontWeight.w600,
-                        color: widget.colorTitle,
-                        fontFamily: 'Poppins',
-                      ),
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  widget.stepDescriptions[widget.step] ?? '',
-                  style: widget.textStyleDescriptions ??
-                      TextStyle(
-                        fontSize: MediaQuery.of(context).size.width * .029,
-                        fontWeight: FontWeight.w400,
-                        color: widget.colorDescriptions,
-                        fontFamily: 'Poppins',
-                      ),
-                ),
-              ],
+          _buildStepInfo(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCircularProgressIndicator(double width, double height) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: RotatedBox(
+        quarterTurns: 90,
+        child: AnimatedBuilder(
+          animation: _progressAnimation,
+          builder: (context, child) {
+            return Transform(
+              transform: widget.fillDirection == FillDirection.clockwise
+                  ? Matrix4.identity()
+                  : Matrix4.rotationY(3.14159),
+              alignment: Alignment.center,
+              child: CircularProgressIndicator(
+                color: widget.progressColor,
+                backgroundColor: widget.backgroundColor,
+                value: _progressAnimation.value,
+                strokeWidth: widget.strokeWidth ?? 6,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProgressText(BuildContext context) {
+    return Positioned(
+      child: Text(
+        '${widget.step} ${widget.betweenStepText} ${widget.totalSteps}',
+        style: widget.textStyleStepper ?? TextStyle(
+          fontSize: MediaQuery.of(context).size.width * .03,
+          fontWeight: FontWeight.w500,
+          color: widget.colorTextStepper,
+          fontFamily: 'Poppins',
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStepInfo(BuildContext context) {
+    return SizedBox(
+      height: widget.height ?? MediaQuery.of(context).size.height * .061,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            widget.stepTitles[widget.step] ?? '',
+            style: widget.textStyleTitle ?? TextStyle(
+              fontSize: MediaQuery.of(context).size.width * .034,
+              fontWeight: FontWeight.w600,
+              color: widget.colorTitle,
+              fontFamily: 'Poppins',
+            ),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            widget.stepDescriptions[widget.step] ?? '',
+            style: widget.textStyleDescriptions ?? TextStyle(
+              fontSize: MediaQuery.of(context).size.width * .029,
+              fontWeight: FontWeight.w400,
+              color: widget.colorDescriptions,
+              fontFamily: 'Poppins',
             ),
           ),
         ],
